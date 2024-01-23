19ace8d78054a1f0f3805c41acb479eb3b1628a3
GermeE13 <151720919+GermeE13@users.noreply.github.com>
2024-01-23 11:45:57 +0100

    Update pfc.py

--- pfc.py
+++ pfc.py
@@ -2,4 +2,32 @@ import random
 
 choix = ['pierre', 'feuille', 'ciseaux']
 bot = random.choice(choix)
-player = input("choisir : [pierre]
+player = input("choisir : [pierre], [feuille], [ciseaux] : ")
+
+if player == bot:
+  print("bot : " + bot + "  player : " + player) 
+  print("égalité")
+
+if player == "pierre":
+    if bot == "feuille":
+        print("bot : " + bot + "  player : " + player) 
+        print("perdu")
+    elif bot == "ciseaux":
+        print("bot : " + bot + "  player : " + player) 
+        print("gagné")
+        
+if player == "feuille":
+    if bot == "ciseaux":
+        print("bot : " + bot + "  player : " + player) 
+        print("perdu")
+    elif bot == "pierre":
+        print("bot : " + bot + "  player : " + player) 
+        print("gagné")
+        
+if player == "ciseaux":
+    if bot == "pierre":
+        print("bot : " + bot + "  player : " + player) 
+        print("perdu")
+    elif bot == "feuille":
+        print("bot : " + bot + "  player : " + player) 
+        print("gagné")
